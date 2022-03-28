b9bea560a1e2f0212efe3ef8726cd2cdef746313
acatoire <acatoire@talend.com>
2022-03-28 16:29:30 +0200

    Add method for file not found safety

--- rest_server.py
+++ rest_server.py
@@ -8,30 +8,31 @@ from flask import Flask, json
 api = Flask(__name__)
 
 
-@api.route('/complexity1_k_h_100', methods=['GET'])
-def complexity1_k_h_100():
+@api.route('/complexity1', methods=['GET'])
+def complexity1():
 
-    file_path = 'json_generator/json_generator/complexity1_k_h_100.json'
-    with open(file_path) as json_file:
-        print(f"REST delivered [{file_path}] with GET")
-        return json.loads(json_file.read())
+    file_path = 'generation/deep-A4L4D3_k_h.json'
+    return get_from_file(file_path)
 
 
-@api.route('/complexity1_k_nh_100', methods=['GET'])
-def complexity1_k_nh_100():
+@api.route('/complexity1n', methods=['GET'])
+def complexity1n():
 
-    content = [f for f in os.listdir(os.path.join(os.getcwd(), "generation"))]
+    file_path = 'generation/deep-A4L4D3_k_nh.json'
+    return get_from_file(file_path)
 
-    print(f"Possibles files:{content}")
-    file_path = 'generation/complexity1_k_nh_100.json'
+
+def get_from_file(file_path):
 
     try:
         with open(file_path) as json_file:
             print(f"REST delivered [{file_path}] with GET")
             return json.loads(json_file.read())
     except FileNotFoundError:
+        content = [f for f in os.listdir(os.path.join(os.getcwd(), "generation"))]
+        print(f"Possibles files:{content}")
         print(f"REST deliver failed [{file_path}]  not found")
-        return "202"
+        return "400"
 
 
 if __name__ == '__main__':
